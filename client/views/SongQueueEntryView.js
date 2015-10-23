26f4c4340bd0b9db9a33329f9f37b48cc703a626
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-10-23 16:25:56 -0700

    added pseudocode to SongQueueView.js and SongQueueEntryView.js

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,18 +1,21 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-   
+
+  // formats songQueueEntryView on page
   tagName: 'tr',
 
+  // sets the html template
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
+  // runs the dequeue function when user clicks on songQueueEntryView
   events: {
     'click': function() {
-      // this.model.play();
       this.model.dequeue();
     }
   },
 
   render: function(){
+    // renders the SongQueueEntryView elements based on the songModel attributes and html template above
     return this.$el.html(this.template(this.model.attributes));
   }
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,17 +1,26 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
+
+  // adds formatting when view renders
   tagName: 'table',
+
+  // backbone runs this function automatically when new SongQueueView is created
   initialize: function() {
+    // listens for add and remove events (add from AppModel.js and backbone automatically emits remove), then re-renders the SongQueueView
     this.collection.on('add remove', this.render, this);
     this.render();
   },
 
   render: function() {
-
+  // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+  // see http://api.jquery.com/detach/
   this.$el.children().detach();
 
+    // makes "SongQueue" into an html element
+    // then appends songQueueCollection to songQueueView (based on SongQueue model)
     this.$el.html('<th>Song Queue</th>').append(
       this.collection.map(function(song){
+        // creates and renders new SongQueueEntryView based on the SongModel.js
         return new SongQueueEntryView({model: song}).render();
       })
     );
