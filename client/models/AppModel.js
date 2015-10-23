c73e10797684438e5e78668ea8dfeb4d0400f9de
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-10-23 12:14:43 -0700

    added pseudocode to AppModel.js

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -1,8 +1,11 @@
 // App.js - Defines a backbone model class for the whole app.
 var AppModel = Backbone.Model.extend({
 
+  // backbone automatically runs initialize function every time new instance of AppModel
   initialize: function(params){
+    // sets the currentSong attribute to a new SongModel object (see SongModel.js in models folder)
     this.set('currentSong', new SongModel());
+    // sets the songQueue attribute to a new SongQueue object (see SongQueue.js in collections folder)
     this.set('songQueue', new SongQueue());
 
     /* Note that 'this' is passed as the third argument. That third argument is
@@ -12,13 +15,19 @@ var AppModel = Backbone.Model.extend({
     end up referring to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
 
-
+    // listens for the play event emitted/triggered by SongModel.js, then sets the currentSong to the song
     params.library.on('play', function(song){
       this.set('currentSong', song);
     }, this);
+
+    // listens for the enqueue event emitted/triggered by SongModel.js
+    // then gets the songQueue property and adds song to it, triggers add event - see SongQueue.js
     params.library.on('enqueue', function(song){
       this.get('songQueue').add(song);
     }, this);
+
+    // gets songQueue property to listen for the stop event triggered by html audio tag
+    // sets currentSong to nothing to make song stop playing
     this.get('songQueue').on('stop', function(){
       this.set('currentSong', null);
     }, this);
