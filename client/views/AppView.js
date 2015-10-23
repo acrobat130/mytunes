36511b4081228007a4d312f5a7875d1f2e38d59c
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-10-23 15:47:17 -0700

    added pseudocode to AppView.js

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -1,9 +1,13 @@
 // AppView.js - Defines a backbone view class for the whole music app.
 var AppView = Backbone.View.extend({
 
+  // backbone automatically runs initialize when new AppView is created
   initialize: function(params){
+    // creates a new playerView based on the currentSong model that was created in AppModel
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
+    // creates a new libraryView based on the library collection that was created in index.html
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    // creates a new songQueueView based on the songQueue.js collection
     this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
@@ -13,6 +17,7 @@ var AppView = Backbone.View.extend({
     }, this);
   },
 
+// renders the playerView, libraryView, and songQueueView html elements to AppView
   render: function(){
     return this.$el.html([
       this.playerView.$el,
